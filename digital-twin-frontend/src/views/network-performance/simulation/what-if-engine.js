e46fced05c26fee117be46b18c19897f472b7f5c
ShivanshMath <shivansh.mathur@incedoinc.com>
2023-12-13 12:37:21 +0530

    no changes

--- digital-twin-frontend/src/views/network-performance/simulation/what-if-engine.js
+++ digital-twin-frontend/src/views/network-performance/simulation/what-if-engine.js
@@ -66,10 +66,16 @@ const WhatIfEngineNetwork = () => {
   // }]
   // : [];
 
+  const KPIkey = [];
+  const KPIvalue = [];
+
+
   // const ResourceCat = [
   //   "00:00", "02:00", "04:00", "06:00", "08:00",
   //   "10:00", "12:00", "14:00", "16:00", "18:00","20:00","22:00"
   // ];
+
+    
 
   const dataspeedVal = apiResult && 'dataspeed' in apiResult ? parseFloat(apiResult['dataspeed']) : 0;
 
